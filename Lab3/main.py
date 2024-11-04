2a119aad448068a541fd131b1115450f0ce69d8c
baldingTomato <86657058+baldingTomato@users.noreply.github.com>
2024-11-04 17:22:56 +0100

    Create main.py

---
+++ Lab3/main.py
@@ -0,0 +1,23 @@
+import math
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+def forwardPass(wiek, waga, wzrost):
+    hidden1 = (wiek * -0.46122) + (waga * 0.97314) + (wzrost * -0.39203) + 0.80109
+    hidden1_po_aktywacji = sigmoid(hidden1)
+    
+    hidden2 = (wiek * 0.78548) + (waga * 2.10584) + (wzrost * -0.57847) + 0.43529
+    hidden2_po_aktywacji = sigmoid(hidden2)
+    
+    output = (hidden1_po_aktywacji * -0.81546) + (hidden2_po_aktywacji * 1.03775) - 0.2368
+    
+    return output
+
+def main():
+    print(forwardPass(23,75,176))
+    print(forwardPass(50,68,180))
+    
+
+if __name__ == "__main__":
+    main()
